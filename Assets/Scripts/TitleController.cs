d131bb7355423e6d2ba629001cf336f0d85cce64
hirotoe0112 <hirotoe0112@gmail.com>
2020-12-22 00:02:45 +0900

    タイトル画面からのシーン遷移
    2020/12/22
    タイトル画面からイントロ画面とランキング画面への遷移
    共通定数、共通処理クラスの作成
    コルーチンとデリゲード

---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class TitleController : MonoBehaviour
+{
+    /// <summary>
+    /// 背景パネル
+    /// </summary>
+    [SerializeField]
+    private GameObject pnlWrap;
+
+    /// <summary>
+    /// ボタン押下
+    /// </summary>
+    private bool isPushButton;
+
+    /// <summary>
+    /// スタートボタン押下時
+    /// </summary>
+    public void GameStart()
+    {
+        if (!isPushButton)
+        {
+            isPushButton = true;
+
+            //パネルをスライドアウトさせた後、イントロダクション画面を表示
+            GlobalProc globalProc = new GlobalProc();
+            GlobalProc.AfterSlideOut afterSlideOut = () => { SceneManager.LoadScene(GlobalConst.INTRO_SCENE); };
+            StartCoroutine(globalProc.SlideOutPanel(pnlWrap, afterSlideOut));
+        }
+    }
+
+    /// <summary>
+    /// ランキングボタン押下時
+    /// </summary>
+    public void DisplayRanking()
+    {
+        if (!isPushButton)
+        {
+            isPushButton = true;
+
+            //パネルをスライドアウトさせた後、イントロダクション画面を表示
+            GlobalProc globalProc = new GlobalProc();
+            GlobalProc.AfterSlideOut afterSlideOut = () => { SceneManager.LoadScene(GlobalConst.RANKING_SCENE); };
+            StartCoroutine(globalProc.SlideOutPanel(pnlWrap, afterSlideOut));
+        }
+    }
+}
