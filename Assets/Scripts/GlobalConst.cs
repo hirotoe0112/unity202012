c74eb6f8900d3193c6cdb0d1f170a124c3baaed4
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2020-12-22 00:02:45 +0900

    タイトル画面からのシーン遷移
    2020/12/22
    タイトル画面からイントロ画面とランキング画面への遷移
    共通定数、共通処理クラスの作成
    コルーチンとデリゲード

---
+++ Assets/Scripts/GlobalConst.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 共通定数
+/// </summary>
+public class GlobalConst
+{
+    #region スライドアウト関連
+    /// <summary>
+    /// スライドアウトまでの時間
+    /// </summary>
+    public const float BG_SLIDE_DURATIOIN = 0.5f;
+
+    /// <summary>
+    /// 1フレームごとのスライド距離
+    /// </summary>
+    public const float BG_SLIDE_UNIT = 0.1f;
+    #endregion
+
+    #region シーン名
+    /// <summary>
+    /// イントロダクション画面
+    /// </summary>
+    public const string INTRO_SCENE = "IntroScene";
+
+    /// <summary>
+    /// ランキング画面
+    /// </summary>
+    public const string RANKING_SCENE = "RankingScene";
+    #endregion
+}
---
+++ Assets/Scripts/GlobalProc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 共通関数
+/// </summary>
+public class GlobalProc
+{
+    /// <summary>
+    /// スライドアウト後の処理用デリゲード
+    /// </summary>
+    public delegate void AfterSlideOut();
+
+    /// <summary>
+    /// スライドアウト
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator SlideOutPanel(GameObject pnlWrap, AfterSlideOut afterSlideOut)
+    {
+        //開始時間
+        float startTime = Time.time;
+
+        //指定の秒数経過するまで移動
+        while ((Time.time - startTime) < GlobalConst.BG_SLIDE_DURATIOIN)
+        {
+            pnlWrap.GetComponent<RectTransform>().transform.Translate(GlobalConst.BG_SLIDE_UNIT, 0, 0);
+            yield return 0;
+        }
+
+        //スライドアウト後の処理
+        afterSlideOut();
+    }
+
+}
